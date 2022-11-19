f82847d9028a9808945b426946ad52bb778e488f
Jannis Berndt <berndtjannis@gmail.com>
2022-11-19 14:34:07 +0100

    Finish implementation

--- src/main/kotlin/exercise1/TPMMSKotlin.kt
+++ src/main/kotlin/exercise1/TPMMSKotlin.kt
@@ -13,26 +13,28 @@ import kotlin.math.ceil
 @ChosenImplementation(true)
 class TPMMSKotlin(manager: BlockManager, sortColumnIndex: Int) :
         SortOperation(manager, sortColumnIndex) {
-    override fun estimatedIOCost(relation: Relation): Int = TODO()
+    override fun estimatedIOCost(relation: Relation): Int {
+        // If the relation consists of n Blocks, Phase 1 needs 2 I/O-Operations per Block, thus 2*n.
+        // Phase 2 has to read all blocks once, thus 1*n. We assume that the BlockOutput.output-method does not need
+        // I/O-Operations, since there are none specified in the method's documentation. So the I/O Cost
+        // estimates to 3*n under these assumptions.
+        return 3 * relation.estimatedSize
+    }
 
     override fun sort(relation: Relation, output: BlockOutput) {
-        val size: Int = relation.estimatedSize
-        val nLists: Int =
-                ceil(relation.estimatedSize / (blockManager.freeBlocks - 1).toDouble()).toInt()
-        val listSize: Int = ceil(relation.estimatedSize / nLists.toDouble()).toInt()
+        val listSize: Int = blockManager.freeBlocks
+        val nLists: Int = ceil(relation.estimatedSize / listSize.toDouble()).toInt()
+        if(nLists > blockManager.freeBlocks - 1)
+            throw RelationSizeExceedsCapacityException()
 
         // Phase 1
-        // Lade jeweils so viele Tupel, wie in Hauptspeicher passen
-        // □ Sortiere Teilstücke (im Hauptspeicher)
-        // □ Schreibe sortierte Teilstücke auf Festplatte zurück
-        // □ Ergebnis: viele sortierte Teillisten (auf Festplatte)
         val lists = mutableListOf<MutableList<Block>>()
-        var blockPointer = relation.iterator()
-        for (i in 1..nLists-1) {
-            var blocks = mutableListOf<Block>()
+        val blockPointer = relation.iterator()
+        for (i in 0 until nLists) {
+            val blocks = mutableListOf<Block>()
             while (blockManager.freeBlocks > 0 &&
                     blockPointer.hasNext() &&
-                    blockManager.usedBlocks <= listSize) blocks.add(
+                    blockManager.usedBlocks < listSize) blocks.add(
                     blockManager.load(blockPointer.next())
             )
 
@@ -45,40 +47,25 @@ class TPMMSKotlin(manager: BlockManager, sortColumnIndex: Int) :
             val references = mutableListOf<Block>()
             for (block in blocks) {
                 references.add(blockManager.release(block, true)!!)
-                // hier passiert I/O;
             }
             lists.add(references)
         }
 
         // Phase 2:
-        // □ Merge alle sortierten Teillisten in einzige große Liste
-
         val outputBlock: Block = blockManager.allocate(true)
         val blocks = mutableListOf<Block>()
         lists.forEach { blocks.add(blockManager.load(it.first())) }
         val iterators = mutableListOf<Iterator<Tuple>>()
         blocks.forEach { iterators.add(it.iterator()) }
+        val firstTuples = mutableListOf<Tuple>()
+        iterators.forEach { firstTuples.add(it.next()) }
 
         while (lists.size > 0) {
-            val firstTuples = blocks.map { it.first() }
-            // for(i in 0..nLists-1) {
-            //   firstTuples.add(blocks.get(i).get(0));
-            // }
-            // val firstTuples = iterators.withIndex().minBy{ (_, f) -> f.get(sortColumnIndex)!!
-            // }?.index
-            // val ffirstTuples = iterators.withIndex().minBy{ (_, f) -> f }?.index
-            // flights.withIndex().minBy { (_, f) -> f.duration }?.index
-
-            // firstTuples.sortedWith(relation.columns.getColumnComparator(sortColumnIndex));
             var min: Pair<Int, Tuple> = 0 to firstTuples.first()
             for (f in firstTuples.withIndex()) {
-                // if(relation.columns.getColumnType(sortColumnIndex).toComparable(min.second[sortColumnIndex])
-                //  >
-                // relation.columns.getColumnType(sortColumnIndex).toComparable(f.value[sortColumnIndex])
-                //  )
                 if (relation.columns
                                 .getColumnComparator(sortColumnIndex)
-                                .compare(min.second, f.value) == 0
+                                .compare(min.second, f.value) > 0
                 )
                         min = f.index to f.value
             }
@@ -86,22 +73,28 @@ class TPMMSKotlin(manager: BlockManager, sortColumnIndex: Int) :
             if (outputBlock.isFull()) {
                 output.output(outputBlock)
             }
-            outputBlock.append(iterators.get(min.first).next())
-            if (!iterators.get(min.first).hasNext()) {
-                blocks.get(min.first).clear()
-                blocks.get(min.first).close()
-                lists.get(min.first).removeAt(0)
-                if (lists.get(min.first).size > 1) {
-                    blocks.set(min.first, blockManager.load(lists[min.first][0]))
-                    iterators.set(min.first, blocks.get(min.first).iterator())
+            outputBlock.append(firstTuples[min.first])
+            if (!iterators[min.first].hasNext()) {
+                blocks[min.first].clear()
+                blocks[min.first].close()
+                lists[min.first].removeAt(0)
+                if (lists[min.first].isNotEmpty()) {
+                    blocks[min.first] = blockManager.load(lists[min.first].first())
+                    iterators[min.first] = blocks[min.first].iterator()
+                    firstTuples[min.first] = iterators[min.first].next()
                 } else {
                     lists.removeAt(min.first)
                     blocks.removeAt(min.first)
                     iterators.removeAt(min.first)
+                    firstTuples.removeAt(min.first)
                 }
+            } else {
+                firstTuples[min.first] = iterators[min.first].next()
             }
         }
+        if(!outputBlock.isEmpty()) {
+            output.output(outputBlock)
+        }
         blockManager.release(outputBlock, false)
-        println(blockManager.usedBlocks)
     }
 }
